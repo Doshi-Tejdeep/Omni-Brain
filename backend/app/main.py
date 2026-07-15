d22043f82cd8545e757d56d04a9df7a4bf50ffe6
Sauma777555 <151991105+Sauma777555@users.noreply.github.com>
2026-07-15 22:23:52 +0530

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,2 +1,15 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File
 app= FastAPI()
+@app.get("/")
+def home():
+    return {"message": "Welcome to OmniBrain API"}
+
+@app.get("/health")
+def health():
+    return {"status": "healthy"}
+@app.post("/upload")
+async def upload(file: UploadFile = File(...)):
+    return {
+        "filename": file.filename,
+        "type": file.content_type
+    }  
